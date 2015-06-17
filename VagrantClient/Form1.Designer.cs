3bccc18b97ef84c54cd64067fd68180519a683e3
Eugen <euginewm@gmail.com>
2015-06-18 00:10:43 +0300

    initial commit

---
+++ VagrantClient/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace VagrantClient
+{
+    partial class FormVagrantClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormVagrantClient));
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.upToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haltToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.upToolStripMenuItem,
+            this.haltToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem1,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(153, 126);
+            // 
+            // upToolStripMenuItem
+            // 
+            this.upToolStripMenuItem.Name = "upToolStripMenuItem";
+            this.upToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.upToolStripMenuItem.Text = "Up";
+            this.upToolStripMenuItem.Click += new System.EventHandler(this.upToolStripMenuItem_Click);
+            // 
+            // haltToolStripMenuItem
+            // 
+            this.haltToolStripMenuItem.Name = "haltToolStripMenuItem";
+            this.haltToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.haltToolStripMenuItem.Text = "Halt";
+            this.haltToolStripMenuItem.Click += new System.EventHandler(this.haltToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "Box list";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Vagrant Client";
+            this.notifyIcon.Visible = true;
+            // 
+            // FormVagrantClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 41);
+            this.Name = "FormVagrantClient";
+            this.Text = "Vagrant Client";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem upToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haltToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ VagrantClient/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace VagrantClient
+{
+    public partial class FormVagrantClient : Form
+    {
+        public FormVagrantClient()
+        {
+            InitializeComponent();
+
+            String[] boxList = this.getVagrantBoxList();
+            foreach (String value in boxList)
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem();
+                item.Text = value;
+                item.Tag = value;
+                item.Click += new EventHandler(processMenuItem);
+                this.toolStripMenuItem1.DropDownItems.Add(item);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // -----------------------
+        private String[] getVagrantBoxList()
+        {
+            Process cmd = new Process();
+
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.CreateNoWindow = true;
+
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("vagrant box list");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+
+            String line = cmd.StandardOutput.ReadToEnd();
+
+            String[] splitResult = line.Split(new String[] { System.Environment.NewLine }, System.StringSplitOptions.RemoveEmptyEntries);
+            String vagrantDelimiter = "(virtualbox";
+            
+            int counter = 0;
+            foreach (string value in splitResult)
+            {
+                int pos = value.IndexOf(vagrantDelimiter);
+                if (pos > -1)
+                {
+                    counter++;
+                }
+            }
+            int i = 0;
+            String[] boxList = new string[counter];
+            foreach (string value in splitResult)
+            {
+                int pos = value.IndexOf(vagrantDelimiter);
+                if (pos > -1)
+                {
+                    boxList[i++] = value.Substring(0, pos-1); // -1 to "(" character
+                }
+            }
+
+            return boxList;
+        }
+
+        private void processMenuItem(object sender, EventArgs e)
+        {
+            MessageBox.Show(sender.ToString());
+            // todo: vagrant destroy --force; ... update box
+        }
+
+        private String vagrantCommand(String command)
+        {
+            Process cmd = new Process();
+
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.CreateNoWindow = true;
+
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(command);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+
+            return cmd.StandardOutput.ReadToEnd();
+        }
+
+        private void haltToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            vagrantCommand("vagrant halt");
+        }
+
+        private void upToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            vagrantCommand("vagrant up");
+        }
+    }
+}
